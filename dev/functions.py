6ccf0d808f9b958f195bd121bdec5337b00e2600
Adrian <rodriguez.adpo@gmail.com>
2020-07-30 19:46:49 +0200

    Analyse visuelle de la segmentation RFM Connaissance des personas type Projections des clusters sur les plans factoriels Ajout d'une fonction dans la biblio perso

--- dev/functions.py
+++ dev/functions.py
@@ -207,3 +207,24 @@ def get_date_int(df, column):
 def get_month(x): return dt.datetime(x.year, x.month, 1)
 
 def join_rfm(x): return str(x['R']) + str(x['F']) + str(x['M'])
+
+def sortedgroupedbar(ax, x, y, groupby, data=None, legend_anchor=None, width=0.8, **kwargs):
+    order = np.zeros(len(data))
+    df = data.copy()
+    for xi in np.unique(df[x].values):
+        group = data[df[x] == xi]
+        a = group[y].values
+        b = sorted(np.arange(len(a)), key=lambda x:a[x], reverse=True)
+        c = sorted(np.arange(len(a)), key=lambda x:b[x])
+        order[data[x] == xi] = c   
+    df["order"] = order
+    u, df["ind"] = np.unique(df[x].values, return_inverse=True)
+    step = width / len(np.unique(df[groupby].values))
+    for xi, grp in df.groupby(groupby):
+        ax.bar(grp["ind"] - width/2. + grp["order"]*step + step/2.,
+               grp[y], width=step, label=xi, **kwargs)
+    ax.legend(title=groupby, bbox_to_anchor=legend_anchor)
+    ax.set_xticks(np.arange(len(u)))
+    ax.set_xticklabels(u)
+    ax.set_xlabel(x)
+    ax.set_ylabel(y)
